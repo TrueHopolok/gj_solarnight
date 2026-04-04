094defbfc0a8de6f6ad14c7e67a496a27b502dcd
wailer <andrypirk@gmail.com>
2026-04-04 20:37:46 +0300

    feature: canon

---
+++ scenes/building/tower/canonball.gd
@@ -0,0 +1,30 @@
+extends AnimatableBody2D
+
+
+const DAMAGE: int = 3
+const SPEED: float = 100.0
+
+
+var target: Node2D = null
+
+
+func _physics_process(delta: float) -> void:
+	if not is_instance_valid(target):
+		target = Tower.pick_target(global_position)
+
+	if not is_instance_valid(target):
+		queue_free()
+		return
+
+	var dir := global_position.direction_to(target.global_position)
+
+	var collision := move_and_collide(dir * SPEED * delta)
+	if collision == null:
+		return
+
+	var obj: Node2D = collision.get_collider()
+	if obj.has_method("damage") and obj.has_method("is_dead") and not obj.is_dead():
+		obj.damage(DAMAGE)
+		queue_free()
+	else:
+		add_collision_exception_with(obj)
---
+++ scenes/building/tower/tower.gd
@@ -0,0 +1,71 @@
+class_name Tower
+extends Node2D
+
+
+const SUN_MULTIPLIER: float = 1.5
+
+@export var reload_time: float = 3.0
+@export var projectile: PackedScene
+
+
+var _reload_left: float
+var _energry: bool = false
+var _under_sun: bool = false
+
+
+func _ready() -> void:
+	_reload_left = randf_range(reload_time, reload_time * 2)
+
+
+func _physics_process(delta: float) -> void:
+	if not _energry:
+		return
+
+	if _under_sun:
+		_reload_left -= delta * 1.5
+	else:
+		_reload_left -= delta
+
+	if _reload_left <= 0:
+		shoot()
+
+
+static func pick_target(pos: Vector2) -> Node2D:
+	var tree: SceneTree = Engine.get_main_loop()
+	var enemies: Array[Node] = tree.get_nodes_in_group(&"enemy")
+	if enemies.is_empty():
+		return null
+
+	var closest: Node2D = null
+	var best_dist := INF
+
+	for enemy: Node2D in enemies:
+		if enemy.has_method(&"is_dead") and enemy.is_dead():
+			continue
+		var dist := enemy.global_position.distance_squared_to(pos)
+		if dist < best_dist:
+			closest = enemy
+			best_dist = dist
+
+	return closest
+
+
+func shoot() -> void:
+	var target: Node2D = pick_target(global_position)
+	if target == null:
+		return
+
+	_reload_left = reload_time
+
+	var inst := projectile.instantiate()
+	get_parent().add_child(inst)
+	inst.set(&"global_transform", global_transform)
+	inst.set(&"target", target)
+
+
+func set_sun_state(v: bool) -> void:
+	_under_sun = v
+
+
+func set_light_state(v: bool) -> void:
+	_energry = v
