7030c7057330271a886484fd70241c57fb5ec3ad
wailer <andrypirk@gmail.com>
2026-04-05 17:01:19 +0300

    fix: canonbal disapearing too early

--- scenes/building/tower/canonball.gd
+++ scenes/building/tower/canonball.gd
@@ -1,21 +1,27 @@
 extends AnimatableBody2D
 
 
+const FAR_AWAY: float = 600
 const DAMAGE: int = 3
 const SPEED: float = 100.0
 
 
+var last_dir: Vector2
 var target: Node2D = null
 
 
 func _physics_process(delta: float) -> void:
-	if not is_instance_valid(target):
+	if position.length_squared() > FAR_AWAY * FAR_AWAY:
 		queue_free()
 		return
 
-	var dir := global_position.direction_to(target.global_position)
+	if is_instance_valid(target):
+		last_dir = global_position.direction_to(target.global_position)
+	elif last_dir.is_zero_approx():
+		queue_free()
+		return
 
-	var collision := move_and_collide(dir * SPEED * delta)
+	var collision := move_and_collide(last_dir * SPEED * delta)
 	if collision == null:
 		return
 
